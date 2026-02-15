515ad79799bddebe19a99b4f677f052eee4bd501
DanCpp <danila.ushakov.08@mail.ru>
2026-02-15 17:18:15 +0700

    gc

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,25 @@
 #include "gc.hpp"
 
 struct Example {
-    int a;
+    int* a;
     int b;
     int c;
-
-    void print() {
-        std::cout << a << b << c << std::endl;
-    }
 };
 
 void memory_leak() {
     void* bebebebebbeeb = GC::instance.allocate(502);
 }
 
+void another_memory_leak() {
+    Example* a = (Example*)GC::instance.allocate(100);
+}
+
 int main(void) {
     void* a = GC::instance.allocate(4);
     memory_leak();
     void* b = GC::instance.allocate(1022);
+    another_memory_leak();
+    void* c = GC::instance.allocate(1000);
 
     return 0;
 }
