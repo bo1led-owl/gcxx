4b651f8f04265a602c6484be31b5538a10054846
DanCpp <danila.ushakov.08@mail.ru>
2026-02-15 15:40:28 +0700

    GC??????

---
+++ src/gc.hpp
@@ -0,0 +1,56 @@
+#include <stack>
+#include <deque>
+#include <bitset>
+
+#include "mallocator.hpp"
+#include "alloc.hpp"
+
+#define HEAP_SIZE 1 << 30;
+#define BITSET_SIZE static_cast<size_t>(HEAP_SIZE / GC::Alloc::MIN_SIZE);
+
+namespace GC {
+  class GCollector {
+    using uintptr = unsigned long;
+    using uint = unsigned int;
+    using m_stack = std::stack<void*, std::deque<void*, MLC::Alloc<void*>>>;
+
+    GC::Alloc allocator;
+    std::bitset<BITSET_SIZE> reachable;
+    void* stack_begin;
+    uint allocations_count;
+
+    template<std::output_iterator ItOut>
+    ItOut GC_scan_registers(ItOut stack);
+
+    template <std::output_iterator ItOut>
+    ItOut GC_scan_stack(ItOut stack);
+
+    void memory_dfs(m_stack& stack);
+
+    void GC_sweep();
+
+    void GC_scan();
+
+    public:
+
+    GCollector() : allocator(HEAP_SIZE), allocations_count(0) {
+      stack_begin = __builtin_frame_address(0);
+    }
+
+
+    void* allocate(size_t size) {
+      allocations_count++;
+      if (allocations_count) { GC_scan(); }
+      return allocator.allocate(size);
+    }
+
+    void deallocate(void* ptr) {
+      allocator.deallocate(ptr);
+    }
+
+  }
+
+
+  static GCollector instance;
+
+} // namespace GarbageCollector
