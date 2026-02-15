8f95b26138cea346c4937a9e0b0a35dada7a2fb4
evlc <dodgedodgedoggy@gmail.com>
2026-02-15 15:35:50 +0700

    main test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 
-int main(void) {
-  std::cout << "bebebebe\n";
+#include "alloc.hpp"
 
-  return 0;
+struct Example {
+    int a;
+    int b;
+    int c;
+
+    void print() {
+        std::cout << a << b << c << std::endl;
+    }
+};
+
+int main(void) {
+    auto gpa = GC::Alloc(4096);
+    Example* a = (Example*)gpa.allocate(sizeof(Example));
+    a->a = 1;
+    a->b = 2;
+    a->c = 3;
+    Example* b = (Example*)gpa.allocate(sizeof(Example));
+    b->a = 4;
+    b->b = 5;
+    b->c = 6;
+    Example* c = (Example*)gpa.allocate(sizeof(Example));
+    c->a = 7;
+    c->b = 8;
+    c->c = 9;
+
+    std::cout << "all allocated\n";
+
+    a->print();
+    b->print();
+    c->print();
+
+    gpa.deallocate(a);
+    gpa.deallocate(b);
+    gpa.deallocate(c);
+
+    return 0;
 }
