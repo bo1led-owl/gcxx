6c4896e3575cb220093289bfeaeb4d822360db29
evlc <dodgedodgedoggy@gmail.com>
2026-02-15 19:16:47 +0700

    degooglify

--- example/test.cpp
+++ example/test.cpp
@@ -1,32 +1,40 @@
-#include <gtest/gtest.h>
-
+#include <cassert>
+#include <climits>
 #include <cstdlib>
+#include <ctime>
+#include <set>
 
 #include "avl.hpp"
 
-TEST(AvlTest, LinearInsertion) {
+#define TEST(a, b, content) void a##_##b() content
+#define ASSERT_TRUE(a) assert(a);
+#define ASSERT_FALSE(a) assert(!a);
+#define ASSERT_EQ(a, b) assert(a == b);
+#define ASSERT_NEQ(a, b) assert(a != b);
+
+TEST(AvlTest, LinearInsertion, {
     AVL a;
     for (int i = 0; i < 90; i++) {
         ASSERT_TRUE(a.insert(90 - 1 - i));
     }
-}
+});
 
-TEST(AvlTest, ReversedLinearInsertion) {
+TEST(AvlTest, ReversedLinearInsertion, {
     AVL a;
     for (int i = 0; i < 90; i++) {
         ASSERT_TRUE(a.insert(i));
     }
-}
+});
 
-TEST(AvlTest, LinearReinsertion) {
+TEST(AvlTest, LinearReinsertion, {
     AVL a;
     ASSERT_TRUE(a.insert(10));
     for (int i = 0; i < 90; i++) {
         ASSERT_FALSE(a.insert(10));
     }
-}
+});
 
-TEST(AvlTest, LinearDeletion) {
+TEST(AvlTest, LinearDeletion, {
     AVL a;
     for (int i = 0; i < 90; i++) {
         ASSERT_TRUE(a.insert(i));
@@ -37,9 +45,9 @@ TEST(AvlTest, LinearDeletion) {
     for (int i = 0; i < 90; i++) {
         ASSERT_FALSE(a.remove(i));
     }
-}
+});
 
-TEST(AvlTest, ReversedLinearDeletion) {
+TEST(AvlTest, ReversedLinearDeletion, {
     AVL a;
     for (int i = 0; i < 90; i++) {
         ASSERT_TRUE(a.insert(i));
@@ -50,9 +58,9 @@ TEST(AvlTest, ReversedLinearDeletion) {
     for (int i = 0; i < 90; i++) {
         ASSERT_FALSE(a.remove(90 - 1 - i));
     }
-}
+});
 
-TEST(AvlTest, LinearFind) {
+TEST(AvlTest, LinearFind, {
     AVL a;
     for (int i = 0; i < 90; i++) {
         ASSERT_TRUE(a.insert(i));
@@ -66,76 +74,57 @@ TEST(AvlTest, LinearFind) {
     for (int i = 0; i < 90; i++) {
         ASSERT_EQ(a.find(i), INT_MIN);
     }
-}
+});
 
-TEST(AvlTest, MinMaxSingleElement) {
+TEST(AvlTest, MinMaxSingleElement, {
     AVL a;
     a.insert(1);
-    EXPECT_EQ(a.min(), 1);
-    EXPECT_EQ(a.max(), 1);
-}
+    ASSERT_EQ(a.min(), 1);
+    ASSERT_EQ(a.max(), 1);
+});
 
-TEST(AvlTest, MinMaxAfterOperations) {
+TEST(AvlTest, MinMaxAfterOperations, {
     AVL a;
     a.insert(5);
     a.insert(3);
     a.insert(7);
     a.insert(2);
     a.insert(4);
-    EXPECT_EQ(a.min(), 2);
-    EXPECT_EQ(a.max(), 7);
+    ASSERT_EQ(a.min(), 2);
+    ASSERT_EQ(a.max(), 7);
     a.remove(2);
-    EXPECT_EQ(a.min(), 3);
+    ASSERT_EQ(a.min(), 3);
     a.remove(7);
-    EXPECT_EQ(a.max(), 5);
-}
+    ASSERT_EQ(a.max(), 5);
+});
 
-TEST(AvlTest, SelectOnEmpty) {
+TEST(AvlTest, SelectOnEmpty, {
     AVL a;
-    EXPECT_EQ(a.select(1), INT_MIN);
-    EXPECT_EQ(a.select(2), INT_MIN);
-}
+    ASSERT_EQ(a.select(1), INT_MIN);
+    ASSERT_EQ(a.select(2), INT_MIN);
+});
 
-TEST(AvlTest, SelectSingle) {
-    AVL a;
-    a.insert(1);
-    EXPECT_EQ(a.select(0), 1);
-    EXPECT_EQ(a.select(2), INT_MIN);
-}
-
-TEST(AvlTest, RankOnEmpty) {
-    AVL a;
-    EXPECT_EQ(a.rank(1), 0);
-}
-
-TEST(AvlTest, RankSingle) {
+TEST(AvlTest, SelectSingle, {
     AVL a;
     a.insert(1);
-    EXPECT_EQ(a.rank(0), 0);
-    EXPECT_EQ(a.rank(1), 0);
-    EXPECT_EQ(a.rank(2), 1);
-}
+    ASSERT_EQ(a.select(0), 1);
+    ASSERT_EQ(a.select(2), INT_MIN);
+});
 
-TEST(AvlTest, RankMultiple) {
+TEST(AvlTest, RankOnEmpty, {
     AVL a;
-    std::vector<int> vals = {5, 3, 8, 1, 4, 7, 9};
-    for (int v : vals) {
-        a.insert(v);
-    }
-    EXPECT_EQ(a.rank(0), 0);
-    EXPECT_EQ(a.rank(1), 0);
-    EXPECT_EQ(a.rank(2), 1);
-    EXPECT_EQ(a.rank(3), 1);
-    EXPECT_EQ(a.rank(4), 2);
-    EXPECT_EQ(a.rank(5), 3);
-    EXPECT_EQ(a.rank(6), 4);
-    EXPECT_EQ(a.rank(7), 4);
-    EXPECT_EQ(a.rank(8), 5);
-    EXPECT_EQ(a.rank(9), 6);
-    EXPECT_EQ(a.rank(10), 7);
-}
+    ASSERT_EQ(a.rank(1), 0);
+});
 
-TEST(AvlTest, SetFuzz) {
+TEST(AvlTest, RankSingle, {
+    AVL a;
+    a.insert(1);
+    ASSERT_EQ(a.rank(0), 0);
+    ASSERT_EQ(a.rank(1), 0);
+    ASSERT_EQ(a.rank(2), 1);
+});
+
+TEST(AvlTest, SetFuzz, {
     AVL a;
     std::set<int> s;
     std::srand(std::time(0));
@@ -145,18 +134,32 @@ TEST(AvlTest, SetFuzz) {
         int val = rand() % 90;
         if (op == 0) {
             bool inserted = a.insert(val);
-            EXPECT_EQ(inserted, s.insert(val).second);
+            ASSERT_EQ(inserted, s.insert(val).second);
         } else if (op == 1) {
             bool removed = a.remove(val);
-            EXPECT_EQ(removed, s.erase(val) == 1);
+            ASSERT_EQ(removed, s.erase(val) == 1);
         } else {
             int found = a.find(val);
             auto it = s.find(val);
-            if (it != s.end())
-                EXPECT_EQ(found, val);
-            else
-                EXPECT_EQ(found, INT_MIN);
+            if (it != s.end()) {
+                ASSERT_EQ(found, val);
+            } else {
+                ASSERT_EQ(found, INT_MIN);
+            }
         }
-        EXPECT_TRUE(a.verify());
+        ASSERT_TRUE(a.verify());
     }
+});
+
+int main(void) {
+    AvlTest_LinearInsertion();
+    AvlTest_LinearDeletion();
+    AvlTest_ReversedLinearDeletion();
+    AvlTest_ReversedLinearInsertion();
+    AvlTest_MinMaxAfterOperations();
+    AvlTest_MinMaxSingleElement();
+    AvlTest_RankOnEmpty();
+    AvlTest_SelectOnEmpty();
+    AvlTest_SelectSingle();
+    AvlTest_SetFuzz();
 }
